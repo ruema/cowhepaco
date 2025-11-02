4c57dcbd1fa3d75196d74be0cbb30a0de5a6e46c
RueMa <RueMa@nowhere>
2025-11-02 23:04:13 +0100

    compare wheel to conda

---
+++ src/cowhepaco/__main__.py
@@ -0,0 +1,96 @@
+import re
+import os
+import json
+import argparse
+import urllib.request
+import zipfile
+from .conda import iter_files
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def compare(files, wheel):
+    names_seen = set()
+    errors = []
+    for file in files:
+        if "site-packages" in file.name.split("/"):
+            name = file.name.partition("site-packages/")[-1]
+            names_seen.add(name)
+            try:
+                data = wheel.open(name).read()
+            except KeyError:
+                if file.name.endswith('.pyc'):
+                    continue
+                errors.append(("not found", name))
+            else:
+                if file.read() != data:
+                    errors.append(("differ", name))
+        else:
+            errors.append(("outside", file.name))
+    not_found = set(
+        info.filename
+        for info in wheel.filelist
+        if not info.is_dir()
+    ) - names_seen
+    for file in not_found:
+        errors.append(("additional", file))
+    return [(mode, name) for mode, name in errors if not name.split('/')[0].endswith('.dist-info')]
+
+
+def get_pypi_wheel_url(package_name, package_version, tag):
+    package_name_normalized = re.sub("[-._]+", "_", package_name).lower()
+    url = f'https://pypi.org/simple/{package_name_normalized}'
+    request = urllib.request.Request(
+         url, headers={
+             "Accept": "application/vnd.pypi.simple.v1+json"
+         }
+    )
+    filename = f"{package_name_normalized}-{package_version}-{tag}.whl"
+    response = urllib.request.urlopen(request)
+    files = json.loads(response.read())['files']
+    for file in files:
+        print(file["filename"], filename)
+        if file["filename"] == filename:
+            return file 
+
+def get_wheel_filename(conda_package_name):
+    package_name = None
+    version = None
+    tag = None
+    files = iter_files(conda_package_name)
+    for file in files:
+        if file.name.endswith("/METADATA"):
+            lines = file.read().decode().split('\n')
+            for line in lines:
+                if line.startswith("Name:"):
+                    package_name = line.split(':',1)[1].strip()
+                elif line.startswith("Version:"):
+                    version = line.split(':',1)[1].strip()
+                    break
+        if file.name.endswith("/WHEEL"):
+            lines = file.read().decode().split('\n')
+            for line in lines:
+                if line.startswith("Tag:"):
+                    tag = line.split(':',1)[1].strip()
+    return package_name, version, tag
+
+
+def main():
+    """Main function of the script."""
+    parser = argparse.ArgumentParser(description="Compares conda and wheel packages.")
+    parser.add_argument("conda_package_name", help="The name of the package.")
+    args = parser.parse_args()
+    package_name, version, tag = get_wheel_filename(args.conda_package_name)
+    wheel_info = get_pypi_wheel_url(package_name, version, tag)
+    filename = os.path.basename(wheel_info["filename"])
+    response = urllib.request.urlopen(wheel_info["url"])
+    data = response.read()
+    with open(filename, 'wb') as file:
+        file.write(data)
+    files = iter_files(args.conda_package_name)
+    wheel = zipfile.ZipFile(filename)
+    print(compare(files, wheel))
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/cowhepaco/conda.py
@@ -0,0 +1,54 @@
+import os
+import platform
+import tarfile
+import backports.zstd as zstd
+import zipfile
+
+SYSTEM = {
+    "linux": "linux",
+    "darwin": "osx",
+    "windows": "win",
+    "freebsd": "freebsd",
+    "emscripten": "emscripten",
+    "wasi": "wasi",
+}
+
+MACHINE = {
+    "x86_64": "64",
+    "amd64": "64",
+    "aarch64": "aarch64",
+    "arm64": "arm64",
+}
+
+
+def get_conda_platform():
+    """Determine the conda platform string."""
+    system = platform.system().lower()
+    machine = platform.machine().lower()
+    return f"{SYSTEM[system]}-{MACHINE[machine]}"
+
+
+def iter_files(package_filename):
+    if package_filename.endswith(".conda"):
+        conda = zipfile.ZipFile(package_filename)
+        for fileinfo in conda.filelist:
+            if fileinfo.filename.startswith('pkg-'):
+                break
+        else:
+            raise ValueError("pkg not found")
+        pkg = conda.open(fileinfo)
+        if fileinfo.filename.endswith('.zst'):
+            pkg = zstd.open(pkg)
+        tar = tarfile.open(fileobj=pkg, mode="r|*")
+    else:
+        tar = tarfile.open(package_filename, mode="r|*")
+    for info in tar:
+        if info.name.startswith('info/'):
+            continue
+        if info.isfile():
+            file = tar.extractfile(info)
+            file.raw.name = info.name
+            yield file
+        else:
+            print(info)
+
